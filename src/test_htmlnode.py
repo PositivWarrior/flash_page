8484c4d5e231fdfca8628426eee39e9856baa1ad
Kacper Margol <kacpermargol@gmail.com>
2026-03-20 12:23:49 +0100

    feat: implement base HTMLNode and LeafNode classes with their respective unit tests.

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -25,6 +25,23 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html_raises(self):
         node = HTMLNode()
         with self.assertRaises(NotImplementedError):
+            node.to_html()
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_raw_text(self):
+        node = LeafNode(None, "Just some raw text.")
+        self.assertEqual(node.to_html(), "Just some raw text.")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
             node.to_html()
 
 if __name__ == "__main__":
