818e25cf385c07505b684622a249a10d74c68edf
Kacper Margol <kacpermargol@gmail.com>
2026-03-22 09:23:31 +0100

    feat: Introduce TextNode class and `text_node_to_html_node` function with corresponding unit tests.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -28,6 +28,28 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             repr(node), 
             "TextNode(Test, italic, https://google.com)"
+        )
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text")
+
+    def test_image(self):
+        node = TextNode("Alt text", TextType.IMAGE, "https://www.boot.dev/img.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev/img.png", "alt": "Alt text"},
         )
 
 if __name__ == "__main__":
