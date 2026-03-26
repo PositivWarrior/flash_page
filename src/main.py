4b91512eefc284da744c7365ce31eaaa0c26b580
Kacper Margol <kacpermargol@gmail.com>
2026-03-26 11:37:57 +0100

    feat: Implement a static site generator with markdown to HTML conversion, template processing, and a local server for generated content.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,31 @@
 import os
 import shutil
 from textnode import TextNode, TextType
+from block_markdown import extract_title, markdown_to_html_node
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as f:
+        markdown_content = f.read()
+
+    with open(template_path, "r") as f:
+        template = f.read()
+
+    node = markdown_to_html_node(markdown_content)
+    html_content = node.to_html()
+
+    title = extract_title(markdown_content)
+
+    full_html = template.replace("{{ Title }}", title)
+    full_html = full_html.replace("{{ Content }}", html_content)
+
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir != "":
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(dest_path, "w") as f:
+        f.write(full_html)
 
 def copy_static_files_to_public(source, destination):
     if os.path.exists(destination):
@@ -33,7 +58,13 @@ def main():
 
     print("Starting static sync...")
     copy_static_files_to_public(static_path, public_path)
-    print("Sync complete!")
+
+    print("Generating index page...")
+    generate_page(
+        "content/index.md",
+        "template.html",
+        "public/index.html"
+    )
 
 if __name__ == "__main__":
     main()
