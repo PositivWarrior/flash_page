f5ee0009f5da26e60067867c81d5d3d10f9641f4
Kacper Margol <kacpermargol@gmail.com>
2026-03-26 10:40:45 +0100

    feat: Implement a static file synchronization script, add initial styling and an image asset, and update gitignore to exclude the public directory.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,39 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
+def copy_static_files_to_public(source, destination):
+    if os.path.exists(destination):
+        print(f"Deleting directory: {destination}")
+        shutil.rmtree(destination)
+
+    print(f"Creating directory: {destination}")
+    os.mkdir(destination)
+
+    recursive_copy(source, destination)
+
+def recursive_copy(src, dst):
+    items = os.listdir(src)
+
+    for item in items:
+        source_path = os.path.join(src, item)
+        dest_path = os.path.join(dst, item)
+
+        if os.path.isfile(source_path):
+            print(f"Copying file: {source_path} -> {dest_path}")
+            shutil.copy(source_path, dest_path)
+        else:
+            print(f"Creating directory: {dest_path}")
+            os.mkdir(dest_path)
+            recursive_copy(source_path, dest_path)
+
 def main():
-    node = TextNode("Hello", TextType.NORMAL)
-    print(node)
+    static_path = "./static"
+    public_path = "./public"
+
+    print("Starting static sync...")
+    copy_static_files_to_public(static_path, public_path)
+    print("Sync complete!")
 
 if __name__ == "__main__":
     main()
