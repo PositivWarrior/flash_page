e65cf69dc9c46820699cf9f1a142de74762df868
Kacper Margol <kacpermargol@gmail.com>
2026-03-27 11:08:55 +0100

    feat: Implement a static site generator and create an initial "Tolkien Fan Club" website with various pages, styling, and images.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Zamień 'flash_page' na nazwę swojego repozytorium na GitHubie
+python3 src/main.py "/flash_page/"
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
+import sys
 import os
 import shutil
 from textnode import TextNode, TextType
 from block_markdown import extract_title, markdown_to_html_node
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as f:
@@ -14,11 +15,13 @@ def generate_page(from_path, template_path, dest_path):
 
     node = markdown_to_html_node(markdown_content)
     html_content = node.to_html()
-
     title = extract_title(markdown_content)
 
     full_html = template.replace("{{ Title }}", title)
     full_html = full_html.replace("{{ Content }}", html_content)
+
+    full_html = full_html.replace('href="/', f'href="{basepath}')
+    full_html = full_html.replace('src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir != "":
@@ -52,7 +55,7 @@ def recursive_copy(src, dst):
             os.mkdir(dest_path)
             recursive_copy(source_path, dest_path)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     entries = os.listdir(dir_path_content)
 
     for entry in entries:
@@ -62,21 +65,26 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(from_path):
             if entry.endswith(".md"):
                 dest_html_path = dest_path.replace(".md", ".html")
-                generate_page(from_path, template_path, dest_html_path)
+                generate_page(from_path, template_path, dest_html_path, basepath)
         else:
             os.makedirs(dest_path, exist_ok=True)
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 def main():
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+
     static_path = "./static"
-    public_path = "./public"
+    docs_path = "./docs"
     content_path = "./content"
     template_path = "./template.html"
 
     print("Starting sync and generation...")
-    copy_static_files_to_public(static_path, public_path)
+    copy_static_files_to_public(static_path, docs_path)
 
-    generate_pages_recursive(content_path, template_path, public_path)
+    generate_pages_recursive(content_path, template_path, docs_path, basepath)
 
     print("Build finieshed successfully!")
 
